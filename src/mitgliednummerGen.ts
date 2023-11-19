9a72fb575009bf22c43d36a388d72fc468759a38
l.eschweiler <l.eschweiler@novacapta.de>
2023-11-19 13:05:08 +0100

    bugfix

--- src/mitgliednummerGen.ts
+++ src/mitgliednummerGen.ts
@@ -1,3 +1,3 @@
 export function mitgliednummerGen(vorname: string, nachname: string){
-  return vorname.charAt(0) + nachname.charAt(0) + (1000 + Math.floor(Math.random() * Math.floor(9000))) + String.fromCharCode(97 + Math.floor(Math.random() * Math.floor(26)));
+  return nachname.charAt(0) + vorname.charAt(0) + (1000 + Math.floor(Math.random() * Math.floor(9000))) + String.fromCharCode(97 + Math.floor(Math.random() * Math.floor(26)));
 }
